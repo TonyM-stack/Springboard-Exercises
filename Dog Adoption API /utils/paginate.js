4d9777288875d40bc0491cd20ec1562b5950f087
TonyM <tony@tmartinphoto.com>
2025-10-01 14:38:36 -0500

    Delete Dog Adoption API  directory

--- Dog Adoption API /utils/paginate.js
+++
@@ -1,7 +0,0 @@
-function buildPagination({ page = 1, limit = 10 }) {
-  const p = Math.max(parseInt(page, 10) || 1, 1);
-  const l = Math.min(Math.max(parseInt(limit, 10) || 10, 1), 100);
-  return { page: p, limit: l, skip: (p - 1) * l };
-}
-
-module.exports = { buildPagination };
